708f201597424faa3cc40c74b61855f1f5ed3c5a
zqz <zqz@zhaoqize.com>
2016-08-23 23:02:01 +0800

    添加css3特效

--- components/head/index.jsx
+++ components/head/index.jsx
@@ -33,12 +33,35 @@ var Header = React.createClass({
 	render:function(){
 		return (
 			<header>
+				 <div className="wrap">
+		            <div className="cube">
+		                <div className="front">
+		                    Zlog
+		                </div>
+		                <div className="back">
+		                    Zlog
+		                </div>
+		                <div className="top">
+		                    Zlog
+		                </div>
+		                <div className="bottom">
+		                    Zlog
+		                </div>
+		                <div className="left">
+		                    Zlog
+		                </div>
+		                <div className="right">
+		                    Zlog
+		                </div>
+		            </div>
+		        </div>
 				<h2>ZBlog 个人博客分享平台</h2>
 				<div className="header_hover" ref="regAndLogin">
 					<a onClick={ this.login }>登陆</a>
 					<a onClick={ this.reg } style={{ marginLeft:20 }}>注册</a>
 				</div>
 				<div ref="userInfo" style={{ display:'none' }}>
+					<a>写博客</a>
 					<a>
 						<span> {this.state.name} </span>
 					</a>
