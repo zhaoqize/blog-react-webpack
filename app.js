9ff7f490c38b22c8e131985cd90d3dc4989bc75e
zqz <zqz@zhaoqize.com>
2016-09-22 21:35:21 +0800

    使用ueditor

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import route from './route/routes.jsx';
 import  './static/css/default.css';
 import  './static/css/header.css';
 import  './static/css/main.css';
-import  './static/css/rtc.css';
 
 window.rootURL = 'http://localhost:80';
 
